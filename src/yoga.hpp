06d199136b86cf6cbc9e195bda229a206162e4fe
Florian Weber <florian.weber@sfz-bw.de>
2013-12-11 01:35:24 +0100

    add license (GPL3 or later)

--- src/yoga.hpp
+++ src/yoga.hpp
@@ -1,6 +1,24 @@
 #ifndef YOGA_INCLUDE_GUARD_YOGA_HPP
 #define YOGA_INCLUDE_GUARD_YOGA_HPP
 
+/*
+ *  libdlo - A modern output-library for C++
+ *  Copyright (C) 2013 Florian Weber <florian.weber at sfz-bw.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include <algorithm>
 #include <array>
